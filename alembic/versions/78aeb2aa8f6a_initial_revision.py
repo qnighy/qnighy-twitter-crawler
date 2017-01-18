94fb2623a26286722f78bbf4dad54c56d95900e7
Masaki Hara <ackie.h.gmai@gmail.com>
2017-01-18 14:09:07 +0900

    Set up alembic for DB schemata versioning.

---
+++ alembic/versions/78aeb2aa8f6a_initial_revision.py
@@ -0,0 +1,24 @@
+"""Initial revision.
+
+Revision ID: 78aeb2aa8f6a
+Revises:
+Create Date: 2017-01-18 12:50:20.584167
+
+"""
+# from alembic import op
+# import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '78aeb2aa8f6a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
